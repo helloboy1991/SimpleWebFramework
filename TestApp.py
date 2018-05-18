31d7881b3a5dad9bedc50ff1e8832436f0b4c96b
helloboy1991 <ybaojia91@gmail.com>
2018-05-18 09:20:10 +0800

    statics route test

--- TestApp.py
+++ TestApp.py
@@ -5,7 +5,8 @@ from SimpleWebServer import WebServer
 
 def main():
     app = WebFramework()
-    app.router.route('/test.html')
+    # app.router.route('/test.html')
+    # app.router.route('/test.js')
     app_server = WebServer(app)
     app_server.server_forever()
 
